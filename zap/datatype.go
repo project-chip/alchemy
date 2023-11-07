556e3795f3e2215077b7e36f1238bca63644d2f4
Hasty Granbery <hasty@granbery.org>
2023-11-07 11:24:09 -0800

    Fix missing data types

--- zap/datatype.go
+++ zap/datatype.go
@@ -3,33 +3,84 @@ package zap
 import "strings"
 
 var matterToZapMap = map[string]string{
-	"bool":        "boolean",
-	"uint8":       "int8u",
-	"uint16":      "int16u",
-	"uint32":      "int32u",
-	"uint64":      "int64u",
-	"int8":        "int8s",
-	"int16":       "int16s",
-	"int32":       "int32s",
-	"int64":       "int64s",
-	"enum8":       "enum8",
-	"enum16":      "enum16",
-	"enum32":      "enum32",
-	"map8":        "bitmap8",
-	"map16":       "bitmap16",
-	"map32":       "bitmap32",
-	"map64":       "bitmap64",
-	"string":      "char_string",
-	"octstr":      "octet_string",
-	"elapsed-s":   "elapsed_s",
-	"epoch-s":     "epoch_s",
-	"epoch-us":    "epoch_us",
-	"fabric-idx":  "fabric_idx",
-	"node-id":     "node_id",
-	"vendor-id":   "vendor_id",
-	"group-id":    "group_id",
-	"endpoint-id": "endpoint_id",
-	"endpoint-no": "endpoint_no",
+	"bool": "boolean",
+
+	"uint8":  "int8u",
+	"uint16": "int16u",
+	"uint24": "int24u",
+	"uint32": "int32u",
+	"uint40": "int40u",
+	"uint48": "int48u",
+	"uint56": "int56u",
+	"uint64": "int64u",
+
+	"int8":  "int8s",
+	"int16": "int16s",
+	"int24": "int24s",
+	"int32": "int32s",
+	"int40": "int40s",
+	"int48": "int48s",
+	"int56": "int56s",
+	"int64": "int64s",
+
+	"enum8":  "enum8",
+	"enum16": "enum16",
+	"enum32": "enum32",
+
+	"map8":  "bitmap8",
+	"map16": "bitmap16",
+	"map32": "bitmap32",
+	"map64": "bitmap64",
+
+	"string": "char_string",
+	"octstr": "octet_string",
+
+	"elapsed-s":  "elapsed_s",
+	"epoch-s":    "epoch_s",
+	"epoch-us":   "epoch_us",
+	"systime_ms": "systime_ms",
+	"systime_us": "systime_us",
+	"posix-ms":   "posix_ms",
+	"utc":        "epoch_s", //Deprecated
+
+	"action-id":      "action_id",
+	"attrib-id":      "attrib_id",
+	"cluster-id":     "cluster_id",
+	"command-id":     "command_id",
+	"data-ver":       "data_ver",
+	"devtype-id":     "devtype_id",
+	"entry-idx":      "entry_idx",
+	"event-id":       "event_id",
+	"event-no":       "event_no",
+	"fabric-id":      "fabric_id",
+	"fabric-idx":     "fabric_idx",
+	"field-id":       "field_id",
+	"group-id":       "group_id",
+	"node-id":        "node_id",
+	"transaction-id": "transaction_id",
+	"vendor-id":      "vendor_id",
+	"endpoint-id":    "endpoint_id",
+	"endpoint-no":    "endpoint_no",
+	"EUI64":          "eui64",
+
+	/* Same on both sides:
+	percent
+	percent100ths
+	tod
+	date
+	temperature
+
+	Not sure:
+	ipadr
+	ipv4adr
+	ipv6adr
+	ipv6pre
+	hwadr
+	semtag
+	namespace
+	tag
+
+	*/
 }
 
 var zapToMatterMap map[string]string
