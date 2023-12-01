cf59cc8d0f7538ab6bdb84ddc41d4b19880dbdf2
Hasty Granbery <hasty@granbery.org>
2023-12-01 11:24:42 -0800

    Support enum and bitmap values as defaults

--- constraint/reference.go
+++ constraint/reference.go
@@ -2,6 +2,7 @@ package constraint
 
 import (
 	"github.com/hasty/alchemy/matter"
+	"github.com/hasty/alchemy/parse"
 )
 
 type ReferenceLimit struct {
@@ -46,11 +47,60 @@ func (c *ReferenceLimit) getReference(cc *matter.ConstraintContext) *matter.Fiel
 	return r
 }
 
-func (c *ReferenceLimit) Default(cc *matter.ConstraintContext) (max matter.ConstraintExtreme) {
-	r := c.getReference(cc)
-	if r == nil || r.Default == "" {
+func (c *ReferenceLimit) getEnumValue(cc *matter.ConstraintContext) (def matter.ConstraintExtreme) {
+	if cc.Field.Type == nil || cc.Field.Type.Model == nil {
 		return
 	}
-	cons := ParseConstraint(r.Default)
-	return cons.Default(cc)
+	en, ok := cc.Field.Type.Model.(*matter.Enum)
+	if !ok {
+		return
+	}
+	for _, v := range en.Values {
+		if v.Name == c.Value {
+			val, err := parse.HexOrDec(v.Value)
+			if err == nil {
+				def = matter.NewUintConstraintExtreme(val, matter.ConstraintExtremeFormatInt)
+				return
+			}
+		}
+	}
+	return
+}
+
+func (c *ReferenceLimit) getBitmapValue(cc *matter.ConstraintContext) (def matter.ConstraintExtreme) {
+	if cc.Field.Type == nil || cc.Field.Type.Model == nil {
+		return
+	}
+	en, ok := cc.Field.Type.Model.(*matter.Bitmap)
+	if !ok {
+		return
+	}
+	for _, v := range en.Bits {
+		if v.Name == c.Value {
+			val, err := v.Mask()
+			if err == nil {
+				def = matter.NewUintConstraintExtreme(val, matter.ConstraintExtremeFormatInt)
+				return
+			}
+		}
+	}
+	return
+}
+
+func (c *ReferenceLimit) Default(cc *matter.ConstraintContext) (max matter.ConstraintExtreme) {
+	r := c.getReference(cc)
+	if r != nil && r.Default != "" {
+		cons := ParseConstraint(r.Default)
+		return cons.Default(cc)
+	}
+	// Couldn't find it as a reference; let's check other possibilities
+	max = c.getEnumValue(cc)
+	if max.Defined() {
+		return
+	}
+	max = c.getBitmapValue(cc)
+	if max.Defined() {
+		return
+	}
+	return
 }
