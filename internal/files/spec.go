003381555e7b2cf03b11db0d654621a994fed20b
Hasty Granbery <hasty@granbery.org>
2024-03-09 18:12:46 -0800

    Spec targeter and parser for pipelines

--- internal/files/spec.go
+++ internal/files/spec.go
@@ -2,6 +2,7 @@ package files
 
 import (
 	"context"
+	"fmt"
 	"io/fs"
 	"log/slog"
 	"path/filepath"
@@ -10,6 +11,7 @@ import (
 
 	"github.com/bytesparadise/libasciidoc/pkg/configuration"
 	"github.com/hasty/alchemy/ascii"
+	"github.com/hasty/alchemy/internal/pipeline"
 	"github.com/hasty/alchemy/matter"
 )
 
@@ -70,4 +72,37 @@ func SplitSpec(docs []*ascii.Doc) (map[matter.DocType][]*ascii.Doc, error) {
 		byType[docType] = append(byType[docType], d)
 	}
 	return byType, nil
+}
+
+func SpecTargeter(specRoot string) func(cxt context.Context) ([]string, error) {
+	return func(cxt context.Context) ([]string, error) {
+		return getSpecPaths(specRoot)
+	}
+}
+
+type SpecParser struct {
+	Spec *matter.Spec
+}
+
+func (sp SpecParser) Name() string {
+	return "Loading spec"
+}
+
+func (sp SpecParser) Type() pipeline.ProcessorType {
+	return pipeline.ProcessorTypeSerial
+}
+
+func (sp *SpecParser) Process(cxt context.Context, input *pipeline.Data[*ascii.Doc], index int32, total int32) (outputs []*pipeline.Data[*ascii.Doc], extras []*pipeline.Data[*ascii.Doc], err error) {
+	err = fmt.Errorf("spec loading must be done serially")
+	return
+}
+
+func (sp *SpecParser) ProcessAll(cxt context.Context, inputs []*pipeline.Data[*ascii.Doc]) (outputs []*pipeline.Data[*ascii.Doc], err error) {
+	docs := make([]*ascii.Doc, 0, len(inputs))
+	for _, i := range inputs {
+		docs = append(docs, i.Content)
+	}
+	sp.Spec, err = ascii.BuildSpec(docs)
+	outputs = inputs
+	return
 }
