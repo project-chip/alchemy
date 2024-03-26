a0b6e6667eb46a0e978d900b2b73928e36be8e6f
Hasty Granbery <hasty@granbery.org>
2024-03-26 12:11:46 -0700

    Include tag in version info
    Include tag in version info

--- config/version.go
+++ config/version.go
@@ -1,20 +1,43 @@
 package config
 
-import "runtime/debug"
+import (
+	"runtime/debug"
+	"strings"
+)
 
 var version string
+
+var Tag string
 
 func Version() string {
 	if len(version) > 0 {
 		return version
 	}
+	var revision string
 	if info, ok := debug.ReadBuildInfo(); ok {
 		for _, setting := range info.Settings {
 			if setting.Key == "vcs.revision" {
-				version = setting.Value
-				return version
+				revision = setting.Value
+				break
 			}
 		}
 	}
-	return "unknown"
+	if len(revision) > 7 {
+		revision = revision[0:7]
+	}
+	var ver strings.Builder
+	if len(Tag) > 0 {
+		ver.WriteString(Tag)
+		if len(revision) > 0 {
+			ver.WriteString(" (")
+			ver.WriteString(revision)
+			ver.WriteRune(')')
+		}
+	} else if len(revision) > 0 {
+		ver.WriteString(revision)
+	} else {
+		ver.WriteString("unknown")
+	}
+	version = ver.String()
+	return version
 }
