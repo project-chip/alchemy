3db5333e78404e77bc58727573cb7e9896504efc
Hasty Granbery <hasty@granbery.org>
2024-06-18 10:38:19 -0700

    Eliminate redundant Reader from spec.Read
    Former-commit-id: 31595797b5c387f2bc883a60b3289ca3e3a8e5f1

--- matter/spec/read.go
+++ matter/spec/read.go
@@ -3,44 +3,30 @@ package spec
 import (
 	"context"
 	"fmt"
+	"io"
 	"os"
-	"path/filepath"
-	"regexp"
 	"strings"
 
-	"github.com/hasty/alchemy/asciidoc"
+	"github.com/hasty/alchemy/asciidoc/parse"
 	"github.com/hasty/alchemy/internal/pipeline"
 )
 
-func readFile(path string) (string, error) {
-	file, err := os.ReadFile(path)
-	if err != nil {
-		return "", err
-	}
-	contents := string(file)
+func ReadFile(path string) (*Doc, error) {
 
-	if filepath.Base(path) == "DoorLock.adoc" {
-		var doorLockPattern = regexp.MustCompile(`\n+\s*[^&\n]+&#8224;\s+`)
-		contents = doorLockPattern.ReplaceAllString(contents, "\n")
-	}
-	return contents, nil
-}
-
-func ReadFile(path string, attributes ...asciidoc.AttributeName) (*Doc, error) {
-
-	contents, err := readFile(path)
+	contents, err := os.Open(path)
 	if err != nil {
 		return nil, err
 	}
-	return Read(contents, path, attributes...)
+	defer contents.Close()
+	return read(contents, path)
 }
 
-func Read(contents string, path string, attributes ...asciidoc.AttributeName) (doc *Doc, err error) {
+func Read(contents string, path string) (doc *Doc, err error) {
+	return read(strings.NewReader(contents), path)
+}
 
-	var d *asciidoc.Document
-
-	d, err = ParseDocument(strings.NewReader(contents), path, attributes...)
-
+func read(r io.Reader, path string) (doc *Doc, err error) {
+	d, err := parse.Reader(path, r)
 	if err != nil {
 		return nil, fmt.Errorf("read error in %s: %w", path, err)
 	}
@@ -49,7 +35,6 @@ func Read(contents string, path string, attributes ...asciidoc.AttributeName) (d
 	if err != nil {
 		return nil, err
 	}
-
 	return doc, nil
 }
 
