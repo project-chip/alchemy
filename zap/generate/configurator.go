b812dfac6891c34f7533133150f5bc17c2c25e9a
Marcos B <15697303+gmarcosb@users.noreply.github.com>
2024-09-30 10:59:01 -0600

    Apply to existing templates
    Co-authored-by: Hasty Granbery <granbery@google.com>

--- zap/generate/configurator.go
+++ zap/generate/configurator.go
@@ -47,9 +47,9 @@ func (tg *TemplateGenerator) renderZapTemplate(configurator *zap.Configurator, x
 	ce := x.SelectElement("configurator")
 	if ce == nil {
 		ce = x.CreateElement("configurator")
-		ce.CreateAttr("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance")
-		ce.CreateAttr("xsi:noNamespaceSchemaLocation", "../../zcl.xsd")
 	}
+	ce.CreateAttr("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance")
+	ce.CreateAttr("xsi:noNamespaceSchemaLocation", "../../zcl.xsd")
 
 	de := ce.SelectElement("domain")
 	if de == nil {
