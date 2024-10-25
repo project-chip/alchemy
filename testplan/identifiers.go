651bcab2172ec145bafb0b9ea9113dfa8420ba45
Hasty Granbery <hasty@granbery.org>
2024-10-25 13:09:54 -0700

    Sync with Cecille's PR, but in handlebars format
    https://github.com/project-chip/alchemy/pull/3

--- testplan/identifiers.go
+++ testplan/identifiers.go
@@ -2,8 +2,8 @@ package testplan
 
 import (
 	"fmt"
+	"strings"
 
-	"github.com/iancoleman/strcase"
 	"github.com/mailgun/raymond/v2"
 	"github.com/project-chip/alchemy/internal/handlebars"
 	"github.com/project-chip/alchemy/matter"
@@ -23,14 +23,14 @@ func entityIdentifier(entity types.Entity) string {
 	case *matter.Field:
 		switch entity.EntityType() {
 		case types.EntityTypeAttribute:
-			return fmt.Sprintf("A_%s", strcase.ToScreamingSnake(entity.Name))
+			return fmt.Sprintf("A_%s", strings.ToUpper(matter.CaseWithSeparator(entity.Name, '_')))
 		}
 	case *matter.Feature:
 		return fmt.Sprintf("F_%s", entity.Code)
 	case *matter.Event:
-		return fmt.Sprintf("E_%s", strcase.ToScreamingSnake(entity.Name))
+		return fmt.Sprintf("E_%s", strings.ToUpper(matter.CaseWithSeparator(entity.Name, '_')))
 	case *matter.Command:
-		return fmt.Sprintf("C_%s", strcase.ToScreamingSnake(entity.Name))
+		return fmt.Sprintf("C_%s", strings.ToUpper(matter.CaseWithSeparator(entity.Name, '_')))
 	}
 	return fmt.Sprintf("UNKNOWN_TYPE_%T", entity)
 }
@@ -48,6 +48,18 @@ func entityIdentifierPaddedHelper(list any, entity types.Entity) raymond.SafeStr
 		}
 	}
 	return raymond.SafeString(fmt.Sprintf("%-*s", longest, entityIdentifier(entity)))
+}
+
+func entityIdentifierPaddingHelper(list any, entity types.Entity) raymond.SafeString {
+	var longest int
+	for entity := range handlebars.Iterate[types.Entity](list) {
+		id := entityIdentifier(entity)
+		if len(id) > longest {
+			longest = len(id)
+		}
+	}
+	id := entityIdentifier(entity)
+	return raymond.SafeString(strings.Repeat(" ", longest-len(id)))
 }
 
 func idHelper(id matter.Number, options *raymond.Options) raymond.SafeString {
