ca5428df277b9242426b9396e6acf438a8157400
Hasty Granbery <hasty@granbery.org>
2024-10-25 13:10:12 -0700

    Allow overrides of test plan paths for cluster groups

--- errata/testplan.go
+++ errata/testplan.go
@@ -1,5 +1,10 @@
 package errata
 
 type TestPlan struct {
-	TestPlanPath string `yaml:"testplan-path,omitempty"`
+	TestPlanPath  string                  `yaml:"testplan-path,omitempty"`
+	TestPlanPaths map[string]TestPlanPath `yaml:"testplan-paths,omitempty"`
+}
+
+type TestPlanPath struct {
+	Path string `yaml:"path,omitempty"`
 }
