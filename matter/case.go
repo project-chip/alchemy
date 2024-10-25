a8aececf63220e5250c0ca61deb94ab68ff4e33c
Hasty Granbery <hasty@granbery.org>
2024-10-25 11:27:36 -0700

    Some comments explaining matter.Case logic

--- matter/case.go
+++ matter/case.go
@@ -17,10 +17,15 @@ func isSeparator(r rune) bool {
 	return r == '.' || r == ' ' || r == '-' || r == '_' || r == '(' || r == ')'
 }
 
+// Case turns a string with spaces into a valid Matter identifier in Pascal Case
+// It is Unicode-aware, and preserves acronyms
+// e.g. "Level control" becomes "LevelControl", "TV set" becomes "TVSet"
 func Case(s string) string {
 	return caseify(s, 0)
 }
 
+// Case turns a string with spaces into a valid Matter identifier, with a custom separator rune
+// e.g. "Level control" becomes "Level-Control", "TV set" becomes "TV-Set"
 func CaseWithSeparator(s string, separator rune) string {
 	return caseify(s, separator)
 }
@@ -29,16 +34,16 @@ func caseify(s string, separator rune) string {
 	runes := []rune(s)
 	b := make([]byte, 0, len(runes))
 	var index int
-	nextUpper := true
+	nextUpper := true // We'll always start with a capital
 	for index < len(runes) {
 		r := runes[index]
 		if unicode.IsUpper(r) {
-			if nextUpper && separator != 0 && len(b) > 0 {
+			if nextUpper && separator != 0 && len(b) > 0 { // If there's a supplied separator, append it, unless the string is empty
 				b = utf8.AppendRune(b, separator)
 			}
 			var end int
 			var endedBySeparator bool
-			for end = index + 1; end < len(runes); end++ {
+			for end = index + 1; end < len(runes); end++ { // Look for a run of upper-case letters
 				if unicode.IsUpper(runes[end]) {
 					if end == len(runes)-1 { // Last rune
 						endedBySeparator = true
@@ -52,13 +57,17 @@ func caseify(s string, separator rune) string {
 				break
 			}
 			if end-index > 1 {
+
 				_, isAcronym := acronyms.Load(string(runes[index:end]))
 				if isAcronym || endedBySeparator {
+					// If this run of upper-case letters is a known acronym, or
+					// it ends the string, preserve it
 					for index < end {
 						b = utf8.AppendRune(b, runes[index])
 						index++
 					}
 				} else {
+					// Otherwise, lower-case the remainder of the block of upper-case letters
 					b = utf8.AppendRune(b, runes[index])
 					index++
 					for index < end-1 {
@@ -69,6 +78,7 @@ func caseify(s string, separator rune) string {
 				}
 				index = end
 			} else {
+				// It's just the one upper-case letter
 				b = utf8.AppendRune(b, runes[index])
 				index++
 			}
