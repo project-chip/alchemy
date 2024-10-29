27e28aedcb1e3e708618956afcdc6e2b4fd417f5
Hasty Granbery <hasty@granbery.org>
2024-10-29 11:03:49 -0700

    Add test cases for CaseWithSeparator

--- matter/case_test.go
+++ matter/case_test.go
@@ -7,6 +7,11 @@ type caseTest struct {
 	output string
 }
 
+type caseSeperatorTest struct {
+	caseTest
+	separator rune
+}
+
 var caseTests = []caseTest{
 	{"TCP Wait", "TCPWait"},
 	{"VendorID", "VendorID"},
@@ -18,6 +23,18 @@ var caseTests = []caseTest{
 	{"Set Tv", "SetTv"},
 }
 
+var caseSeparatorTests = []caseSeperatorTest{
+	{caseTest: caseTest{"TCP Wait", "TCP_Wait"}, separator: '_'},
+	{caseTest: caseTest{"VendorID", "VendorID"}, separator: '_'},
+	{caseTest: caseTest{"VendorID", "VendorID"}, separator: '_'},
+	{caseTest: caseTest{"Vendor Attribute", "Vendor_Attribute"}, separator: '_'},
+	{caseTest: caseTest{"HTTP Request", "HTTP_Request"}, separator: '_'},
+	{caseTest: caseTest{"TV Set", "TV_Set"}, separator: '_'},
+	{caseTest: caseTest{"Set TV", "Set_TV"}, separator: '_'},
+	{caseTest: caseTest{"Set Tv", "Set_Tv"}, separator: '_'},
+	{caseTest: caseTest{"LoggedOut", "LoggedOut"}, separator: '_'},
+}
+
 func TestSuite(t *testing.T) {
 	AddCaseAcronym("ID")
 	for _, ct := range caseTests {
@@ -26,5 +43,10 @@ func TestSuite(t *testing.T) {
 			t.Errorf("casing failed; input %s; expected %s, got %s", ct.input, ct.output, out)
 		}
 	}
-
+	for _, ct := range caseSeparatorTests {
+		out := CaseWithSeparator(ct.input, ct.separator)
+		if out != ct.output {
+			t.Errorf("casing with separator failed; input %s; expected %s, got %s", ct.input, ct.output, out)
+		}
+	}
 }
