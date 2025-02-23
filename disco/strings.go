e258cc076e9da3cca22061471bd20b060b5fd67c
Hasty Granbery <hasty@granbery.org>
2025-02-22 16:27:59 -0800

    Ignore text inside monospace when looking for periods without trailing space

--- disco/strings.go
+++ disco/strings.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/project-chip/alchemy/asciidoc"
 	"github.com/project-chip/alchemy/internal/parse"
+	"github.com/project-chip/alchemy/matter/spec"
 )
 
 var missingSpaceAfterPunctuationPattern = regexp.MustCompile(`([a-z])([.?!,])([A-Z])`)
@@ -21,10 +22,24 @@ func precleanStrings(els asciidoc.Set) {
 }
 
 func (b *Baller) postCleanUpStrings(els asciidoc.Set) {
-	parse.Search(els, func(t *asciidoc.String) parse.SearchShould {
-		if b.options.addSpaceAfterPunctuation {
-			t.Value = missingSpaceAfterPunctuationPattern.ReplaceAllString(t.Value, "$1$2 $3")
+	parse.Search(els, func(el any) parse.SearchShould {
+		se, ok := el.(*spec.Element)
+		if ok {
+			el = se.Base
 		}
+		switch el := el.(type) {
+		case *asciidoc.Monospace, *asciidoc.DoubleMonospace:
+			return parse.SearchShouldSkip
+		case *asciidoc.Table:
+			return parse.SearchShouldSkip
+		case *asciidoc.String:
+			if b.options.addSpaceAfterPunctuation {
+				el.Value = missingSpaceAfterPunctuationPattern.ReplaceAllString(el.Value, "$1$2 $3")
+			}
+		}
+		return parse.SearchShouldContinue
+	})
+	parse.Search(els, func(t *asciidoc.String) parse.SearchShould {
 		if b.options.removeExtraSpaces {
 			t.Value = multipleSpacesPattern.ReplaceAllString(t.Value, "$1 $2")
 		}
