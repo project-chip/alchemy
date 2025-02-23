275b20fa1e196ca6afb63bf4412ad24d94073a05
Hasty Granbery <hasty@granbery.org>
2025-02-22 16:25:42 -0800

    Bump to go 1.24

--- config/version.go
+++ config/version.go
@@ -14,31 +14,33 @@ func Version() string {
 	if len(version) > 0 {
 		return version
 	}
-	var revision string
-	if info, ok := debug.ReadBuildInfo(); ok {
+	info, ok := debug.ReadBuildInfo()
+	if ok {
+		if len(info.Main.Version) > 0 {
+			return info.Main.Version
+		}
+		var revision string
 		for _, setting := range info.Settings {
 			if setting.Key == "vcs.revision" {
 				revision = setting.Value
 				break
 			}
 		}
-	}
-	if len(revision) > 7 {
-		revision = revision[0:7]
-	}
-	var ver strings.Builder
-	if len(Tag) > 0 {
-		ver.WriteString(Tag)
-		if len(revision) > 0 {
-			ver.WriteString(" (")
-			ver.WriteString(revision)
-			ver.WriteRune(')')
+		if len(revision) > 7 {
+			revision = revision[0:7]
 		}
-	} else if len(revision) > 0 {
-		ver.WriteString(revision)
-	} else {
-		ver.WriteString("unknown")
+		if len(Tag) > 0 {
+			var ver strings.Builder
+			ver.WriteString(Tag)
+			if len(revision) > 0 {
+				ver.WriteString(" (")
+				ver.WriteString(revision)
+				ver.WriteRune(')')
+			}
+			return ver.String()
+		} else if len(revision) > 0 {
+			return revision
+		}
 	}
-	version = ver.String()
-	return version
+	return "unknown"
 }
