8067b7d502a3c66c04af304cfeca24e44b697945
Hasty Granbery <hasty@granbery.org>
2025-04-07 16:02:50 -0700

    Add helper for entity name

--- matter/entity.go
+++ matter/entity.go
@@ -1,6 +1,8 @@
 package matter
 
 import (
+	"log/slog"
+
 	"github.com/project-chip/alchemy/asciidoc"
 	"github.com/project-chip/alchemy/matter/types"
 )
@@ -29,4 +31,44 @@ func (e entity) Origin() (path string, line int) {
 
 func (e entity) Cluster() *Cluster {
 	return findCluster(e.parent)
+}
+
+func EntityName(e types.Entity) string {
+	switch entity := e.(type) {
+	case *Cluster:
+		return entity.Name
+	case *Struct:
+		return entity.Name
+	case *Field:
+		return entity.Name
+	case *Event:
+		return entity.Name
+	case *Command:
+		return entity.Name
+	case *Bitmap:
+		return entity.Name
+	case *Enum:
+		return entity.Name
+	case *TypeDef:
+		return entity.Name
+	case *Namespace:
+		return entity.Name
+	case *SemanticTag:
+		return entity.Name
+	case *Constant:
+		return entity.Name
+	case *Feature:
+		return entity.Name()
+	case *Condition:
+		return entity.Feature
+	case *DeviceType:
+		return entity.Name
+	case *ClusterGroup:
+		return entity.Name
+	case Bit:
+		return entity.Name()
+	default:
+		slog.Error("Unknown entity type for name", LogEntity("entity", e))
+		return ""
+	}
 }
