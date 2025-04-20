86f9ef179c55f3598380aebbe2891baf402d8bb8
Hasty Granbery <hasty@granbery.org>
2025-04-20 08:49:41 -0700

    Allow initial workgroup to finish processing queue to avoid double processing dependencies

--- internal/pipeline/parallel.go
+++ internal/pipeline/parallel.go
@@ -20,7 +20,7 @@ var gray = color.New(color.FgBlack).Add(color.Bold)
 
 func Parallel[I, O any](cxt context.Context, options ProcessingOptions, processor IndividualProcessor[I, O], input Map[string, *Data[I]]) (output Map[string, *Data[O]], err error) {
 	total := int32(input.Size())
-	queue := make(chan *Data[I], total)
+
 	var values iter.Seq[*Data[I]]
 	if options.Serial {
 		inputs := DataMapToSlice(input)
@@ -29,24 +29,14 @@ func Parallel[I, O any](cxt context.Context, options ProcessingOptions, processo
 	} else {
 		values = dataMapValues(input)
 	}
-	for value := range values {
-		select {
-		case queue <- value:
-		default:
-			err = fmt.Errorf("queue full")
 
-		}
-		if err != nil {
-			return
-		}
-	}
 	if options.Serial {
-		return processSerial(cxt, processor.Name(), processor.Process, queue, total)
+		return processSerial(cxt, processor.Name(), processor.Process, values, total)
 	}
-	return processParallel(cxt, processor.Name(), processor.Process, queue, total, !options.NoProgress)
+	return processParallel(cxt, processor.Name(), processor.Process, values, total, !options.NoProgress)
 }
 
-func processParallel[I, O any](cxt context.Context, name string, processor IndividualProcess[I, O], queue chan *Data[I], total int32, showProgress bool) (output Map[string, *Data[O]], err error) {
+func processParallel[I, O any](cxt context.Context, name string, processor IndividualProcess[I, O], values iter.Seq[*Data[I]], total int32, showProgress bool) (output Map[string, *Data[O]], err error) {
 
 	processed := NewConcurrentMapPresized[string, bool](int(total))
 	output = NewConcurrentMapPresized[string, *Data[O]](int(total))
@@ -56,67 +46,87 @@ func processParallel[I, O any](cxt context.Context, name string, processor Indiv
 		bar = progressbar.Default(int64(total))
 	}
 	var complete int32
-
-	wg := newWorkGroup()
-	for {
-		var input *Data[I]
-		var done bool
+	queue := make(chan *Data[I], total)
+	for value := range values {
 		select {
-		case input = <-queue:
-			done, _ = processed.LoadOrStore(input.Path, false)
+		case queue <- value:
+			processed.LoadOrStore(value.Path, false)
 		default:
+			err = fmt.Errorf("queue full")
 		}
-		if input == nil {
+		if err != nil {
+			return
+		}
+	}
+
+	for {
+		wg := newWorkGroup()
+		for {
+			var input *Data[I]
+			var done bool
+			select {
+			case input = <-queue:
+				done, _ = processed.LoadOrStore(input.Path, false)
+			default:
+			}
+			if input == nil {
+				break
+			}
+			if done {
+				if bar != nil {
+					_ = bar.Add(1)
+				}
+				continue
+			}
+			wg.run(cxt, func() error {
+				done := atomic.AddInt32(&complete, 1)
+				completed, _ := processed.Load(input.Path)
+				if completed {
+					slog.WarnContext(cxt, "skipping already processed input", slog.String("path", input.Path))
+					return nil
+				}
+				outputs, extras, err := processor(cxt, input, done, total)
+				if err != nil {
+					return err
+				}
+				if bar != nil {
+					bar.Describe(progressFileName(input.Path))
+					_ = bar.Add(1)
+				}
+				processed.Store(input.Path, true)
+				for _, e := range extras {
+					_, loaded := processed.LoadOrStore(e.Path, false)
+					if loaded {
+						slog.WarnContext(cxt, "skipping already queued input", slog.String("path", input.Path))
+						continue
+					}
+					select {
+					case queue <- e:
+						newTotal := atomic.AddInt32(&total, 1)
+						if bar != nil {
+							bar.ChangeMax(int(newTotal))
+						}
+					default:
+						return fmt.Errorf("queue full")
+					}
+				}
+				for _, o := range outputs {
+					_, loaded := output.LoadAndStore(o.Path, o)
+					if loaded {
+						return fmt.Errorf("duplicate path in output: %s", o.Path)
+					}
+				}
+				return nil
+			})
+		}
+		err = wg.Wait()
+		if err != nil {
+			return
+		}
+		if len(queue) == 0 {
 			break
 		}
-		if done {
-			if bar != nil {
-				_ = bar.Add(1)
-			}
-			continue
-		}
-		wg.run(cxt, func() error {
-			done := atomic.AddInt32(&complete, 1)
-			completed, _ := processed.Load(input.Path)
-			if completed {
-				slog.WarnContext(cxt, "skipping already processed input", slog.String("path", input.Path))
-				return nil
-			}
-			outputs, extras, err := processor(cxt, input, done, total)
-			if err != nil {
-				return err
-			}
-			if bar != nil {
-				bar.Describe(progressFileName(input.Path))
-				_ = bar.Add(1)
-			}
-			processed.Store(input.Path, true)
-			for _, e := range extras {
-				_, loaded := processed.LoadOrStore(e.Path, false)
-				if loaded {
-					slog.WarnContext(cxt, "skipping already queued input", slog.String("path", input.Path))
-					continue
-				}
-				select {
-				case queue <- e:
-					newTotal := atomic.AddInt32(&total, 1)
-					if bar != nil {
-						bar.ChangeMax(int(newTotal))
-					}
-				default:
-					return fmt.Errorf("queue full")
-				}
-			}
-			for _, o := range outputs {
-				_, loaded := output.LoadAndStore(o.Path, o)
-				if loaded {
-					return fmt.Errorf("duplicate path in output: %s", o.Path)
-				}
-			}
-			return nil
-		})
 	}
-	err = wg.Wait()
 	return
 }
 
--- internal/pipeline/serial.go
+++ internal/pipeline/serial.go
@@ -2,28 +2,46 @@ package pipeline
 
 import (
 	"context"
-	"fmt"
+	"iter"
 	"log/slog"
 	"os"
 )
 
-func processSerial[I, O any](cxt context.Context, name string, processor IndividualProcess[I, O], queue chan *Data[I], total int32) (output Map[string, *Data[O]], err error) {
+type queue[V any] struct {
+	initial    iter.Seq[V]
+	additional []V
+}
+
+func (q *queue[V]) Next() iter.Seq[V] {
+	return func(yield func(V) bool) {
+		for v := range q.initial {
+			if !yield(v) {
+				return
+			}
+		}
+		for {
+			if len(q.additional) == 0 {
+				return
+			}
+			v := q.additional[0]
+			q.additional = q.additional[1:]
+			if !yield(v) {
+				return
+			}
+		}
+	}
+}
+
+func processSerial[I, O any](cxt context.Context, name string, processor IndividualProcess[I, O], values iter.Seq[*Data[I]], total int32) (output Map[string, *Data[O]], err error) {
 	var counter int32
 	processed := make(map[string]bool, total)
 	output = NewMapPresized[string, *Data[O]](int(total))
 	cyan.Fprintf(os.Stderr, "%s...\n", name)
 
-	for {
-		var input *Data[I]
-		var done bool
-		select {
-		case input = <-queue:
-			done = processed[input.Path]
-		default:
-		}
-		if input == nil {
-			return
-		}
+	queue := &queue[*Data[I]]{initial: values}
+
+	for input := range queue.Next() {
+		done := processed[input.Path]
 		if done {
 			continue
 		}
@@ -41,14 +59,7 @@ func processSerial[I, O any](cxt context.Context, name string, processor Individ
 			if ok {
 				continue
 			}
-			select {
-			case queue <- e:
-				total++
-				processed[e.Path] = false
-			default:
-				err = fmt.Errorf("queue full")
-				return
-			}
+			queue.additional = append(queue.additional, e)
 		}
 		for _, o := range outputs {
 			_, loaded := output.LoadOrStore(o.Path, o)
@@ -58,4 +69,5 @@ func processSerial[I, O any](cxt context.Context, name string, processor Individ
 			}
 		}
 	}
+	return
 }
