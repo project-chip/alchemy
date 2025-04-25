6711f5ae14e1e932158f9256fc8d87dca9a2919a
Hasty Granbery <hasty@granbery.org>
2025-04-25 09:16:24 -0700

    rules.matterlint has moved

--- zap/render/lint.go
+++ zap/render/lint.go
@@ -1,16 +1,29 @@
 package render
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"regexp"
 	"strings"
+
+	"github.com/project-chip/alchemy/internal/files"
 )
 
 var templatePathPattern = regexp.MustCompile(`(?m)^load "../src/app/zap-templates/zcl/data-model/chip/[^.]+\.xml";\n`)
 
-func patchLintBytes(sdkRoot string, files []string) (lintPath string, lintBytes []byte, err error) {
-	lintPath = filepath.Join(sdkRoot, "/scripts/rules.matterlint")
+func patchLintBytes(sdkRoot string, xmlFiles []string) (lintPath string, lintBytes []byte, err error) {
+	lintPath = filepath.Join(sdkRoot, "/.matterlint")
+	exists, _ := files.Exists(lintPath)
+	if !exists {
+		lintPath = filepath.Join(sdkRoot, "/scripts/rules.matterlint")
+		exists, _ = files.Exists(lintPath)
+		if !exists {
+			err = fmt.Errorf("unable to find rules.matterlint in %s or %s", filepath.Join(sdkRoot, "/rules.matterlint"), filepath.Join(sdkRoot, "/scripts/rules.matterlint"))
+			return
+		}
+	}
+
 	lintBytes, err = os.ReadFile(lintPath)
 	if err != nil {
 		return
@@ -19,12 +32,12 @@ func patchLintBytes(sdkRoot string, files []string) (lintPath string, lintBytes 
 
 	newPathMap := make(map[string]struct{})
 
-	for _, f := range files {
+	for _, f := range xmlFiles {
 		newPathMap[`load "../src/app/zap-templates/zcl/data-model/chip/`+filepath.Base(f)+"\";\n"] = struct{}{}
 	}
 
 	matches := templatePathPattern.FindAllStringSubmatch(lint, -1)
-	paths := make([]string, 0, len(files)+len(matches))
+	paths := make([]string, 0, len(xmlFiles)+len(matches))
 	for _, m := range matches {
 		path := m[0]
 		delete(newPathMap, path)
