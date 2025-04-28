a7c84f273fe7f9f5093183ff5e926fd47d03c5c6
Hasty Granbery <hasty@granbery.org>
2025-04-28 11:53:43 -0700

    Set aliases correctly as shorts

--- internal/files/options.go
+++ internal/files/options.go
@@ -1,6 +1,6 @@
 package files
 
 type OutputOptions struct {
-	DryRun bool `default:"false" aliases:"d,dryrun" help:"whether or not to actually output files" group:"Output:"`
-	Patch  bool `default:"false" aliases:"p" help:"generate patch file" group:"Output:"`
+	DryRun bool `default:"false" short:"d" aliases:"dryrun" help:"whether or not to actually output files" group:"Output:"`
+	Patch  bool `default:"false" short:"p" help:"generate patch file" group:"Output:"`
 }
