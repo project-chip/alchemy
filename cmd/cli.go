749d7c10cb94d5e1ed4977eeacdc170665acafde
Hasty Granbery <hasty@granbery.org>
2025-05-09 14:25:52 -0700

    Hidden command to auto-add words to wordlist

--- cmd/cli.go
+++ cmd/cli.go
@@ -20,6 +20,7 @@ var commands struct {
 	TestScript  cli.TestScript  `cmd:"" name:"test-script" aliases:"testscript" help:"create shell python scripts from the spec, optionally filtered to the files specified by filename_pattern" group:"Testing Commands:"`
 	Validate    cli.Validate    `cmd:"" help:"validate the Matter specification object model" group:"Spec Commands:"`
 	Yaml2Python cli.Yaml2Python `cmd:"" name:"yaml-2-python" aliases:"yaml2python" help:"create a shell python script from a test YAML, optionally filtered to the files specified by filename_pattern"  group:"Testing Commands:"`
+	Wordlist    cli.Wordlist    `cmd:"" hidden:"" name:"wordlist" help:"add words to wordlist.txt"`
 	Version     Version         `cmd:"" hidden:"" name:"version" help:"display version number"`
 
 	globalFlags `embed:""`
