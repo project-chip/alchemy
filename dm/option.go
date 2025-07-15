396bc498c41e911e053719cc69e3fb8a31d1e761
Hasty Granbery <hasty@granbery.org>
2025-07-15 11:38:12 -0700

    Add option to allow data model generation to parse cluster hierarchy

--- dm/option.go
+++ dm/option.go
@@ -1,5 +1,6 @@
 package dm
 
 type DataModelOptions struct {
-	DmRoot string `default:"connectedhomeip/data_model/master" aliases:"dmRoot" help:"where to place the data model files" group:"Data Model:"`
+	DmRoot          string `default:"connectedhomeip/data_model/master" aliases:"dmRoot" help:"where to place the data model files" group:"Data Model:"`
+	IgnoreHierarchy bool   `default:"true" help:"don't parse the cluster hierarchy when building data model" group:"Data Model:"`
 }
