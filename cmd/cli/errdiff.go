054d2162da43de7dbe5a37790f9b4d2bfd61f3f9
Arya Hassanli <aryahassanli@google.com>
2025-10-08 19:41:27 +0000

    Log the PR errors to a file

--- cmd/cli/errdiff.go
+++ cmd/cli/errdiff.go
@@ -13,12 +13,13 @@ type ErrDiff struct {
 
 	spec.FilterOptions `embed:""`
 
-	HeadRoot string `name:"head-root" default:"connectedhomeip-spec" help:"the src root of your clone of CHIP-Specifications/connectedhomeip-spec"`
-	BaseRoot string `name:"base-root" default:"connectedhomeip-spec" help:"the src root of your clone of CHIP-Specifications/connectedhomeip-spec"`
+	HeadRoot   string `name:"head-root" default:"connectedhomeip-spec" help:"the src root of your clone of CHIP-Specifications/connectedhomeip-spec"`
+	BaseRoot   string `name:"base-root" default:"connectedhomeip-spec" help:"the src root of your clone of CHIP-Specifications/connectedhomeip-spec"`
+	OutputFile string `name:"output-file" default:"" help:"the file to write the output to"`
 }
 
 func (c *ErrDiff) Run(cc *Context) (err error) {
-	err = errdiff.Pipeline(cc, c.BaseRoot, c.HeadRoot, c.Paths, c.ProcessingOptions)
+	err = errdiff.Pipeline(cc, c.BaseRoot, c.HeadRoot, c.Paths, c.ProcessingOptions, c.OutputFile)
 
 	return
 }
