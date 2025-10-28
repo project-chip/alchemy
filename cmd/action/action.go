2db7edd9711eb8ca475a24f0db7d700b653f801f
Arya Hassanli <aryahassanli@google.com>
2025-10-28 19:36:41 +0000

    Remove action

--- cmd/action/action.go
+++ cmd/action/action.go
@@ -4,6 +4,5 @@ type Action struct {
 	Comment    Comment    `cmd:""  help:"GitHub action for Matter spec documents"`
 	Disco      Disco      `cmd:"" default:"" help:"GitHub action for Matter spec documents"`
 	ZAP        ZAP        `cmd:"" help:"GitHub action for Matter SDK ZAP XML"`
-	ZAPDiff    ZAPDiff    `cmd:"zap-diff" help:"GitHub action for Matter SDK ZAP Diff XML"`
 	MergeGuard MergeGuard `cmd:"" help:"GitHub action to prevent Provisionality and Parse errors to be merged."`
 }
