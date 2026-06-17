4f78aad4295d18f5fc5395d2fca1dea2b7aa44cf
Arya Hassanli <aryahassanli@google.com>
2026-06-17 14:30:14 +0000

    Replace fieldId with id for event fields

--- zap/render/field.go
+++ zap/render/field.go
@@ -13,14 +13,14 @@ import (
 func (cr *configuratorRenderer) setFieldAttributes(fieldElement *etree.Element, parentEntityType types.EntityType, field *matter.Field, fieldSet matter.FieldSet, newlyAdded bool) {
 	switch parentEntityType {
 	case types.EntityTypeEvent:
-		// Remove incorrect attributes from legacy XML, but leave "id" on event for now, as ZAP expects it; once ZAP is reading "fieldId", remove this
 		fieldElement.RemoveAttr("code")
+		fieldElement.RemoveAttr("fieldId")
+		xml.PrependAttribute(fieldElement, "id", field.ID.IntString())
 	default:
-		// Remove incorrect attributes from legacy XML
 		fieldElement.RemoveAttr("code")
 		fieldElement.RemoveAttr("id")
+		xml.PrependAttribute(fieldElement, "fieldId", field.ID.IntString())
 	}
-	xml.PrependAttribute(fieldElement, "fieldId", field.ID.IntString(), "id") // once ZAP is reading "fieldId", remove "id"
 	fieldName := zap.CleanName(field.Name)
 	fieldElement.CreateAttr("name", fieldName)
 	cr.writeDataType(fieldElement, fieldSet, field)
